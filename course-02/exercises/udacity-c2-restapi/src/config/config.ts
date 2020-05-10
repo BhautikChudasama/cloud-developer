9c579727ae31e9e3bb00bc8bc8c389daf6b06dc0
BhautikChudasama <bhautikrchudasama@gmail.com>
2020-05-10 13:35:45 +0530

    added env

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,19 +1,19 @@
 export const config = {
   "dev": {
-    "username": "myadmin",
-    "password": "myadmin123",
-    "database": "udagram-dev",
-    "host": "database-1.cstewxadxfak.us-east-1.rds.amazonaws.com",
+    "username": process.env.USERNAME,
+    "password": process.env.PASSWORD,
+    "database": process.env.DB,
+    "host": process.env.HOST,
     "dialect": "postgres",
     "aws_region": "us-east-1",
     "aws_profile": "default",
-    "aws_media_bucket": "udagram-bhautik-prod"
+    "aws_media_bucket": process.env.BUCKET
   },
   "prod": {
-    "username": "myadmin",
-    "password": "myadmin123",
-    "database": "",
-    "host": "database-1.cstewxadxfak.us-east-1.rds.amazonaws.com",
+    "username": process.env.USERNAME,
+    "password": process.env.PASSWORD,
+    "database": process.env.DB,
+    "host": process.env.HOST,
     "dialect": "postgres"
   },
   "jwt": {
